035ccc75d542088ef52404780625f8aa5cac9fdb
spdavern <sean@lizandsean.com>
2014-06-15 08:47:38 -0700

    Intermediate save - form working.

--- scripts/myApp.js
+++ scripts/myApp.js
@@ -1,18 +1,16 @@
 angular.module('myApp', [])
-	.constant('VERSION', "2.0")
-	.run(function(VERSION, $rootScope){
-	$rootScope.version = VERSION;
-})
-	.controller('madLibsController', function($scope){
-		$scope.words = {
-			femaleName:"",
-			jobTitle:"",
-			tediousTask:"",
-			dirtyTask:"",
-			celebrity:"",
-			uselessSkill:"",
-			obnoxiousCeleb:"",
-			hugeNumber:"",
-			adjective:""
-		}
+	.constant('VERSION', "2.1")
+	.run(function(VERSION, $rootScope) {
+		$rootScope.version = VERSION;
 	})
+	.controller('madLibsCtrl', function($scope) {
+	})
+	.controller('formCtrl', ['$scope', function($scope){
+		$scope.submit = function() {
+			if($scope.myForm.$valid) {
+				console.log('The form is valid');
+			} else {
+				console.log('The form is invalid');
+			}
+		};
+	}]);
