1cf0abb7cc0949203cd7401731f21ee59473b217
spdavern <sean@lizandsean.com>
2014-06-15 17:44:56 -0700

    Intermediate save, Need to get animation working.

--- scripts/myApp.js
+++ scripts/myApp.js
@@ -4,13 +4,23 @@ angular.module('myApp', [])
 		$rootScope.version = VERSION;
 	})
 	.controller('madLibsCtrl', function($scope) {
+		$scope.data = {gender: 'male', heShe: "he", hisHer: "his", showResult: false}
 	})
 	.controller('formCtrl', ['$scope', function($scope){
 		$scope.submit = function() {
-			if($scope.myForm.$valid) {
-				console.log('The form is valid');
+			if($scope.data.gender == 'male') {
+				$scope.data.heShe = "he";
+				$scope.data.hisHer = "his";
 			} else {
-				console.log('The form is invalid');
+				$scope.data.heShe = "she";
+				$scope.data.hisHer	= "her";
+			}
+			if($scope.myForm.$valid) {
+				$scope.showResult = true;
+				console.log('The form is valid', $scope.data, $scope.showResult);
+			} else {
+				$scope.showResult = false;
+				console.log('The form is invalid', $scope.data, $scope.showResult);
 			}
 		};
 	}]);
