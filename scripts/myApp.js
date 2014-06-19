d622c0de81776d08d26cbdd09a0098e39a8693a9
spdavern <sean@lizandsean.com>
2014-06-19 06:47:01 -0700

    Functioning solution.

--- scripts/myApp.js
+++ scripts/myApp.js
@@ -1,26 +1,29 @@
 angular.module('myApp', [])
-	.constant('VERSION', "2.2")
+	.constant('VERSION', "3")
 	.run(function(VERSION, $rootScope) {
 		$rootScope.version = VERSION;
 	})
 	.controller('madLibsCtrl', function($scope) {
 		$scope.data = {gender: 'male', heShe: "he", hisHer: "his", showResult: false}
-	})
-	.controller('formCtrl', ['$scope', function($scope){
 		$scope.submit = function() {
 			if($scope.data.gender == 'male') {
 				$scope.data.heShe = "he";
 				$scope.data.hisHer = "his";
+				$scope.data.himHer = "him"
 			} else {
 				$scope.data.heShe = "she";
 				$scope.data.hisHer	= "her";
+				$scope.data.himHer = "her"
 			};
 			if($scope.myForm.$valid) {
 				$scope.showResult = true;
-				console.log('The form is valid', $scope.data, $scope.showResult);
 			} else {
 				$scope.showResult = false;
-				console.log('The form is invalid', $scope.data, $scope.showResult);
 			};
 		};
-	}]);
+		$scope.return = function() {
+			$scope.submitted = false;
+			$scope.showResult = false;
+		}
+	}
+);
