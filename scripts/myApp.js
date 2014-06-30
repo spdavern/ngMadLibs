790bcd0b67e95b17c49085bdacaf96ed93ea8ab0
spdavern <sean@lizandsean.com>
2014-06-30 09:04:10 -0700

    Initial attempts to add Greensock animations

--- scripts/myApp.js
+++ scripts/myApp.js
@@ -4,7 +4,8 @@ angular.module('myApp', ["ngAnimate"])
 		$rootScope.version = VERSION;
 	})
 	.controller('madLibsCtrl', function($scope) {
-		$scope.data = {gender: 'male', heShe: "he", hisHer: "his", showResult: false}
+		$scope.data = {gender: 'male', heShe: "he", hisHer: "his", showResult: false,
+					   hideWords: false}
 		$scope.submit = function() {
 			if($scope.data.gender == 'male') {
 				$scope.data.heShe = "he";
@@ -15,14 +16,29 @@ angular.module('myApp', ["ngAnimate"])
 				$scope.data.hisHer	= "her";
 				$scope.data.himHer = "her"
 			};
-			if(['a','e','i','o','u'].indexOf($scope.data.jobTitle.charAt(0)) !== -1) {
-				$scope.data.aAn = "an"
-			} else {
-				$scope.data.aAn = "a"
+			if($scope.data.jobTitle !== undefined) {
+				if(['a','e','i','o','u'].indexOf($scope.data.jobTitle.charAt(0)) !== -1) {
+					$scope.data.aAn = "an"
+				} else {
+					$scope.data.aAn = "a"
+				};
 			};
-			if($scope.myForm.$valid) {
-				$scope.showResult = true;
-			};
+			var TextObj = document.getElementById("personNameText");
+			var objX = parseInt(TextObj.getBoundingClientRect().left);
+			var objY = parseInt(TextObj.getBoundingClientRect().top);
+			var fromInput = document.myForm.personName;
+			var fromX = parseInt(fromInput.getBoundingClientRect().left);
+			var fromY = parseInt(fromInput.getBoundingClientRect().top);
+			var btnLocation = document.myForm.submitButton;
+			var btnX = parseInt(btnLocation.getBoundingClientRect().left);
+			var btnY = parseInt(btnLocation.getBoundingClientRect().top);
+			$scope.data.hideWords = false;
+			TweenLite.to(TextObj, 2, {x: fromX + "px", y: fromY + "px"});
+			// TweenLite.fromTo(obj, 2, {x: fromX + "px", y: fromY + "px"}, 
+			// 						 {x: btnX + "px", y: btnY + "px"});
+			// if($scope.myForm.$valid) {
+			// 	$scope.showResult = true;
+			// };
 		};
 		$scope.return = function() {
 			$scope.submitted = false;
